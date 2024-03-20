5988235c456d0d85b32e3eb86605f2a2f5591c26
h3110 <desenvolvimento15@dl.com.br>
2024-03-20 10:18:43 -0300

    vs01

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const text = document.querySelector('.text');
 const progressBar = document.querySelector('.progress-bar');
 
-text.textContent = 'Mês do consumidor '.repeat(1500)
+text.textContent = 'Mês do consumidor '.repeat(1500);
 
 calculationProgressBar();
 
 document.addEventListener('scroll', calculationProgressBar);
 
-function calculationProgressBar () {
-  const body = document.documentElement;
-  const height = body.scrollHeight - body.clientHeight;
+function calculationProgressBar() {
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   const percent = (window.pageYOffset / height) * 100;
-  
+
   progressBar.style.width = percent + '%';
-};
+}
